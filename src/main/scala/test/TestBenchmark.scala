8370c3d03930b69624fece50b234d36e5130bc99
Cary Robbins <carymrobbins@gmail.com>
2018-04-20 14:56:44 -0500

    Try manual newtype with unapply Some

--- src/main/scala/test/TestBenchmark.scala
+++ src/main/scala/test/TestBenchmark.scala
@@ -6,40 +6,54 @@ import org.openjdk.jmh.annotations.Benchmark
 /**
   * Based on [[https://github.com/ktoso/sbt-jmh/blob/master/sbt-jmh-tester/src/main/scala/test/TestBenchmark.scala]]
   */
+object types {
 
-
-object `package` {
   @newtype case class Foo(private[this] val x: String)
   object Foo {
     def unapply(foo: Foo): Option[String] = Some(foo.asInstanceOf[String])
   }
 
   // Handwritten newtype to avoid ClassTag unapply slowing things down
-  type Foo2 = Foo2.Type
-  object Foo2 {
-    type Type <: Base with Tag
-    type Base <: Any
-    def apply(x: String): Foo2 = x.asInstanceOf[Foo2]
-    def unapply(x: Foo2): Unapply = new Unapply(x.asInstanceOf[String])
-  }
+  type Manual = Manual.Type
+
+  // Handwritten newtype to avoid ClassTag unapply slowing things down
+  type ManualUnapplyValueClass = ManualUnapplyValueClass.Type
 
   case class Bar(x: String)
 }
 
-// Ideally these would live in the `object Foo2`; however, jmh seems to throw
-// an InternalError for Malformed class name, so putting here instead.
-trait Tag
-final class Unapply(val get: String) extends AnyVal {
-  def isEmpty: Boolean = false
+import types._
+
+object Manual {
+  type Type <: Base with Tag
+  trait Tag
+  type Base <: Any
+  def apply(x: String): Manual = x.asInstanceOf[Manual]
+  def unapply(x: Manual): Some[String] = Some(x.asInstanceOf[String])
+}
+
+object ManualUnapplyValueClass {
+  type Type <: Base with Tag
+  trait Tag
+  type Base <: Any
+  def apply(x: String): ManualUnapplyValueClass = x.asInstanceOf[ManualUnapplyValueClass]
+  def unapply(x: ManualUnapplyValueClass): Unapply = new Unapply(x.asInstanceOf[String])
+  final class Unapply(val get: String) extends AnyVal {
+    def isEmpty: Boolean = false
+  }
 }
 
 class TestBenchmark {
+
   @Benchmark
   def testCaseClass = Bar("1") match { case Bar(x) => x }
 
   @Benchmark
-  def testNewType = Foo("1") match { case Foo(x) => x }
+  def testManualSimpleUnapply = Manual("1") match { case Manual(x) => x }
 
   @Benchmark
-  def testNewType2 = Foo2("1") match { case Foo2(x) => x }
+  def testManualUnapplyValueClass = ManualUnapplyValueClass("1") match { case ManualUnapplyValueClass(x) => x }
+
+  @Benchmark
+  def testNewTypeSimpleUnapply = Foo("1") match { case Foo(x) => x }
 }
