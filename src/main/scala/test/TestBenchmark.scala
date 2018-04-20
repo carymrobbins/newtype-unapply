a0f45e9f7182292f9100985b7e3aac7875dcf635
Cary Robbins <carymrobbins@gmail.com>
2018-04-20 12:42:36 -0500

    Benchark Unapply value class

--- src/main/scala/test/TestBenchmark.scala
+++ src/main/scala/test/TestBenchmark.scala
@@ -14,7 +14,23 @@ object `package` {
     def unapply(foo: Foo): Option[String] = Some(foo.asInstanceOf[String])
   }
 
+  // Handwritten newtype to avoid ClassTag unapply slowing things down
+  type Foo2 = Foo2.Type
+  object Foo2 {
+    type Type <: Base with Tag
+    type Base <: Any
+    def apply(x: String): Foo2 = x.asInstanceOf[Foo2]
+    def unapply(x: Foo2): Unapply = new Unapply(x.asInstanceOf[String])
+  }
+
   case class Bar(x: String)
+}
+
+// Ideally these would live in the `object Foo2`; however, jmh seems to throw
+// an InternalError for Malformed class name, so putting here instead.
+trait Tag
+final class Unapply(val get: String) extends AnyVal {
+  def isEmpty: Boolean = false
 }
 
 class TestBenchmark {
@@ -23,4 +39,7 @@ class TestBenchmark {
 
   @Benchmark
   def testNewType = Foo("1") match { case Foo(x) => x }
+
+  @Benchmark
+  def testNewType2 = Foo2("1") match { case Foo2(x) => x }
 }
